691216222910059312459917053b17aed2acd4d9
Rein <reinX244@gmail.com>
2023-02-27 16:29:00 +0800

    Added JavaVariables Program

---
+++ JavaVariables/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Using int data type
+		int x; // declaring a variable
+		x = 123; // assigning a variable
+		int y = 456; // initializing a variable
+
+		System.out.println("x contains: " + x);
+		System.out.println("y contains: " + y);
+
+		// Using the long data type to store large integers
+		long longNum = 123456789;
+		System.out.println("longNum: " + longNum);
+
+		// Using float and double to assign decimals
+		float floatNum = 1.5673467f;
+		System.out.println("floatNum: " + floatNum);
+
+		double doubleNum = 2.566785673453463;
+		System.out.println("doubleNum: " + doubleNum);
+
+		// booleans
+		boolean isTrue = true;
+		boolean isFalse = false;
+
+		System.out.println("isTrue: " + isTrue);
+		System.out.println("isFalse: " + isFalse);
+
+		// chars
+		char mySymbol = '$';
+		System.out.println("mySymbol: " + mySymbol);
+
+		// String
+		String myName = "Rein";
+		System.out.println("Hello " + myName);
+
+	}
+
+}
