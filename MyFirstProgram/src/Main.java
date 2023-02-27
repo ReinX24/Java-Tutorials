5b234e957f0a40a9a4c01590a0b4414769b2e004
Rein <reinX244@gmail.com>
2023-02-27 15:54:29 +0800

    Added HelloWorld & MyFirstProgram Java Projects

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("I love chicken!"); // ln means line new, brings text to next line
+		System.out.println("It is really good\n"); // we could also use an escape sequence
+		System.out.println("\tTab time!"); // \t is an escape sequence of tab
+		System.out.println("\\This has backslashes!\\"); // printing backslashes
+		
+		// Single line comment
+		
+		/*
+		 * Multiple line comment
+		 * This
+		 * is 
+		 * a
+		 * comment
+		 */
+		
+	}
+
+}
