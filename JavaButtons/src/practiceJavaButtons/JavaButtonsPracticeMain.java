dae305b20146c2502bb679aef376abf1c4998caa
Rein <reinX244@gmail.com>
2023-03-18 17:29:48 +0800

    Fixed problem in practiceJavaButtons Bug that does not show the button's images and text fixed.

--- JavaButtons/src/practiceJavaButtons/JavaButtonsPracticeMain.java
+++ JavaButtons/src/practiceJavaButtons/JavaButtonsPracticeMain.java
@@ -1,7 +1,7 @@
 package practiceJavaButtons;
 
 public class JavaButtonsPracticeMain {
-	
+
 	public static void main(String[] args) {
 		new PracticeFrame(); // executes the creation of a JFrame in another class
 	}
--- JavaButtons/src/practiceJavaButtons/PracticeFrame.java
+++ JavaButtons/src/practiceJavaButtons/PracticeFrame.java
@@ -18,7 +18,7 @@ public class PracticeFrame extends JFrame implements ActionListener {
 	JButton fighterButton;
 	JButton wizardButton;
 	JButton healerButton;
-	
+
 	JLabel fighterPortrait;
 	JLabel wizardPortrait;
 	JLabel healerPortrait;
@@ -36,83 +36,64 @@ public class PracticeFrame extends JFrame implements ActionListener {
 		// Adding 3 buttons to our JFrame, each button represents a class (Fighter,
 		// Wizard, Healer)
 		fighterButton = new JButton();
+		fighterButton.setText("[Fighter]");
+		fighterButton.setFont(new Font("Arial", Font.BOLD, 16));
 		fighterButton.setBounds(100, 100, 200, 300); // x y width height
 		fighterButton.setFocusable(false);
 		fighterButton.addActionListener(this);
 		fighterButton.setBackground(new Color(255, 128, 0));
 		fighterButton.setBorder(BorderFactory.createEtchedBorder());
 		fighterButton.setLayout(new BorderLayout());
+		fighterButton.setHorizontalTextPosition(JButton.CENTER);
+		fighterButton.setVerticalTextPosition(JButton.TOP);
 		this.add(fighterButton);
 
 		wizardButton = new JButton();
+		wizardButton.setText("[Wizard]");
+		wizardButton.setFont(new Font("Arial", Font.BOLD, 16));
 		wizardButton.setBounds(400, 100, 200, 300);
 		wizardButton.setFocusable(false);
 		wizardButton.addActionListener(this);
 		wizardButton.setBackground(new Color(0, 128, 128));
 		wizardButton.setBorder(BorderFactory.createEtchedBorder());
 		wizardButton.setLayout(new BorderLayout());
+		wizardButton.setHorizontalTextPosition(JButton.CENTER);
+		wizardButton.setVerticalTextPosition(JButton.TOP);
 		this.add(wizardButton);
 
 		healerButton = new JButton();
+		healerButton.setText("[Healer]");
+		healerButton.setFont(new Font("Arial", Font.BOLD, 16));
 		healerButton.setBounds(700, 100, 200, 300);
 		healerButton.setFocusable(false);
 		healerButton.addActionListener(this);
 		healerButton.setBackground(new Color(248, 240, 227));
 		healerButton.setBorder(BorderFactory.createEtchedBorder());
 		healerButton.setLayout(new BorderLayout());
+		healerButton.setHorizontalTextPosition(JButton.CENTER);
+		healerButton.setVerticalTextPosition(JButton.TOP);
 		this.add(healerButton);
 
 		// Adding JLabels to each of our buttons
 
 		// Fighter Class
-		JLabel fighterLabel = new JLabel();
-		fighterLabel.setText("[Fighter]");
-		fighterLabel.setFont(new Font("Arial", Font.BOLD, 16));
-		fighterLabel.setVisible(true);
-
 		ImageIcon fighterIcon = new ImageIcon("warriorSword.png");
-		fighterLabel.setHorizontalAlignment(JLabel.CENTER);
-		fighterLabel.setVerticalAlignment(JLabel.CENTER);
-		fighterLabel.setHorizontalTextPosition(JLabel.CENTER);
-		fighterLabel.setVerticalTextPosition(JLabel.TOP);
-		fighterLabel.setIcon(fighterIcon);
-
-		fighterButton.add(fighterLabel);
+		fighterButton.setIcon(fighterIcon);
 
 		// Wizard Class
-		JLabel wizardLabel = new JLabel();
-		wizardLabel.setText("[Wizard]");
-		wizardLabel.setFont(new Font("Arial", Font.BOLD, 16));
-		wizardLabel.setVisible(true);
-
 		ImageIcon wizardIcon = new ImageIcon("wizardStaff.png");
-		wizardLabel.setHorizontalAlignment(JLabel.CENTER);
-		wizardLabel.setVerticalAlignment(JLabel.CENTER);
-		wizardLabel.setHorizontalTextPosition(JLabel.CENTER);
-		wizardLabel.setVerticalTextPosition(JLabel.TOP);
-		wizardLabel.setIcon(wizardIcon);
-
-		wizardButton.add(wizardLabel);
+		wizardButton.setIcon(wizardIcon);
 
 		// Healer Class
-		JLabel healerLabel = new JLabel();
-		healerLabel.setText("[Healer]");
-		healerLabel.setFont(new Font("Arial", Font.BOLD, 16));
-		healerLabel.setVisible(true);
-
 		ImageIcon healerIcon = new ImageIcon("healerStaff.png");
-		healerLabel.setHorizontalAlignment(JLabel.CENTER);
-		healerLabel.setVerticalAlignment(JLabel.TOP);
-		healerLabel.setHorizontalTextPosition(JLabel.CENTER);
-		healerLabel.setVerticalTextPosition(JLabel.CENTER);
-		healerLabel.setIcon(healerIcon);
+		healerButton.setIcon(healerIcon);
 
-		healerButton.add(healerLabel);
-
-		// TODO: move the photos outside of the buttons, putting them inside the buttons
-		// does not show them sometimes
-		
-		
+		// Fighter Portrait
+		fighterPortrait = new JLabel();
+		fighterPortrait.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+		fighterPortrait.setBounds(100, 500, 800, 400);
+		fighterPortrait.setVisible(true);
+		this.add(fighterPortrait);
 
 	}
 
@@ -121,6 +102,7 @@ public class PracticeFrame extends JFrame implements ActionListener {
 		// if the Fighter button is the one pressed
 		if (arg0.getSource() == fighterButton) {
 			System.out.println("[Fighter Chosen!]");
+			fighterPortrait.setVisible(true);
 		} else if (arg0.getSource() == wizardButton) {
 			System.out.println("[Wizard Chosen!]");
 		} else if (arg0.getSource() == healerButton) {
