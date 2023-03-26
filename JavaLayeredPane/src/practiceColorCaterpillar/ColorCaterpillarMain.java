6d8018096604079a7e6514c8d09377d4ac709148
Rein <reinX244@gmail.com>
2023-03-26 08:16:48 +0800

    practiceColorCaterpillar finished

--- JavaLayeredPane/src/practiceColorCaterpillar/ColorCaterpillarMain.java
+++ JavaLayeredPane/src/practiceColorCaterpillar/ColorCaterpillarMain.java
@@ -56,32 +56,74 @@ public class ColorCaterpillarMain {
 		greenLabel.setBounds(60, 60, 100, 100);
 		greenLabel.setOpaque(true);
 		greenLabel.setBackground(Color.GREEN);
+		
+		JLabel blueLabel = new JLabel();
+		blueLabel.setBounds(70, 70, 100, 100);
+		blueLabel.setOpaque(true);
+		blueLabel.setBackground(Color.BLUE);
+		
+		JLabel blackLabel = new JLabel();
+		blackLabel.setBounds(80, 80, 100, 100);
+		blackLabel.setOpaque(true);
+		blackLabel.setBackground(Color.BLACK);
 
+		JLabel grayLabel = new JLabel();
+		grayLabel.setBounds(90, 90, 100, 100);
+		grayLabel.setOpaque(true);
+		grayLabel.setBackground(Color.GRAY);
+		
+		JLabel lightGrayLabel = new JLabel();
+		lightGrayLabel.setBounds(100, 100, 100, 100);
+		lightGrayLabel.setOpaque(true);
+		lightGrayLabel.setBackground(Color.LIGHT_GRAY);
+		
+		JLabel darkGrayLabel = new JLabel();
+		darkGrayLabel.setBounds(110, 110, 100, 100);
+		darkGrayLabel.setOpaque(true);
+		darkGrayLabel.setBackground(Color.DARK_GRAY);
+		
+		JLabel orangeLabel = new JLabel();
+		orangeLabel.setBounds(120, 120, 100, 100);
+		orangeLabel.setOpaque(true);
+		orangeLabel.setBackground(Color.ORANGE);
+
+		JLabel pinkLabel = new JLabel();
+		pinkLabel.setBounds(130, 130, 100, 100);
+		pinkLabel.setOpaque(true);
+		pinkLabel.setBackground(Color.PINK);
+		
 		// Adding JLabels to our JLayeredPane
 		/*
 		 * Add Colors:
 		 * Red X
 		 * Green X
-		 * Blue
+		 * Blue X
 		 * Yellow X
 		 * Cyan X
 		 * Magenta X
 		 * White X
-		 * Black
-		 * Gray
-		 * Light Gray
-		 * Dark Gray
-		 * Orange
-		 * Pink
+		 * Black X
+		 * Gray X
+		 * Light Gray X
+ 		 * Dark Gray X
+		 * Orange X
+		 * Pink X
 		 * 
-		 * */
+		 */
 		mainLayeredPane.add(whiteLabel, Integer.valueOf(0));
 		mainLayeredPane.add(yellowLabel, Integer.valueOf(1));
 		mainLayeredPane.add(magentaLabel, Integer.valueOf(2));
 		mainLayeredPane.add(cyanLabel, Integer.valueOf(3));
 		mainLayeredPane.add(redLabel, Integer.valueOf(4));
 		mainLayeredPane.add(greenLabel, Integer.valueOf(5)); // you can go beyond 5 components
-
+		mainLayeredPane.add(blueLabel, Integer.valueOf(6));
+		mainLayeredPane.add(blackLabel, Integer.valueOf(7));
+		mainLayeredPane.add(grayLabel, Integer.valueOf(8));
+		mainLayeredPane.add(lightGrayLabel, Integer.valueOf(9));
+		mainLayeredPane.add(darkGrayLabel, Integer.valueOf(10));
+		mainLayeredPane.add(orangeLabel, Integer.valueOf(11));
+		mainLayeredPane.add(pinkLabel, Integer.valueOf(12));
+		
 		mainFrame.add(mainLayeredPane);
 		mainFrame.setVisible(true);
 	}
