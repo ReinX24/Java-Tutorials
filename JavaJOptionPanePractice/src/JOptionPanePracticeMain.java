518d448e9893f3a16ce1f5efc71998a3589007b4
Rein <reinX244@gmail.com>
2023-03-28 20:30:55 +0800

    Added JavaJOptionPanePractice (in progress)

---
+++ JavaJOptionPanePractice/src/JOptionPanePracticeMain.java
@@ -0,0 +1,49 @@
+import javax.swing.JOptionPane;
+
+public class JOptionPanePracticeMain {
+
+	public static void main(String[] args) {
+
+//		JOptionPane.showMessageDialog(null, "This is a plain message!", "[Plain Message]", JOptionPane.PLAIN_MESSAGE);
+
+//		JOptionPane.showMessageDialog(null, "This is an information message!", "[Information Message]", JOptionPane.INFORMATION_MESSAGE);
+
+//		JOptionPane.showMessageDialog(null, "This is a questiom message!", "[Question Message]",
+//				JOptionPane.QUESTION_MESSAGE);
+
+//		JOptionPane.showMessageDialog(null, "This is a warning message!", "[Warning Message]",
+//				JOptionPane.WARNING_MESSAGE);
+
+//		JOptionPane.showMessageDialog(null, "This is an error message!", "[Error Message]", JOptionPane.ERROR_MESSAGE);
+
+//		String userNameString = JOptionPane.showInputDialog("[Enter your name]");
+//		JOptionPane.showMessageDialog(null, "Hello " + userNameString + "!", "[Greeting Box]",
+//				JOptionPane.INFORMATION_MESSAGE);
+
+		int userChoice = JOptionPane.showConfirmDialog(null, "[Do you like programming?]", "[Programming Query]",
+				JOptionPane.YES_NO_CANCEL_OPTION);
+		switch (userChoice) {
+		case 0:
+			JOptionPane.showMessageDialog(null, "I like programming too! :D", "[Happy Response]",
+					JOptionPane.INFORMATION_MESSAGE);
+			break;
+
+		case 1:
+			JOptionPane.showMessageDialog(null, "Aw man :(", "[Sad Response]", JOptionPane.ERROR_MESSAGE);
+			break;
+
+		case 2:
+			JOptionPane.showMessageDialog(null, "Ok then, keep your secrets", "[Cancal Response]",
+					JOptionPane.QUESTION_MESSAGE);
+			break;
+
+		default:
+			JOptionPane.showMessageDialog(null, "Exited window!", "[Exit Response]", JOptionPane.WARNING_MESSAGE);
+			break;
+		}
+
+		// TODO: showOptionDialog
+
+	}
+
+}
