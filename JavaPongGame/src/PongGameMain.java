aa383ba35c2c3ed1c13e6bfb3923cb3512b61628
Rein <reinX244@gmail.com>
2023-05-10 21:17:10 +0800

    Added notes in PongGameMain, potential additional features

--- JavaPongGame/src/PongGameMain.java
+++ JavaPongGame/src/PongGameMain.java
@@ -4,6 +4,17 @@ public class PongGameMain {
 
 		new PongGameFrame(); // calling our PongGameFrame constructor
 
+		/*
+		 * Ideas for features to be added:
+		 * 
+		 * - Main Menu before starting the game 
+		 * - Change paddle colors using JColorChooser class 
+		 * - Change background color using JColorChooser class 
+		 * - Change ImageIcon of our program 
+		 * - First to a chosen point in in main menu wins
+		 * 
+		 */
+
 	}
 
 }
