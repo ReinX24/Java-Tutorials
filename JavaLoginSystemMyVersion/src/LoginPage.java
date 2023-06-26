ce47c6d986d3601ccc7ce214b4b0f4e9a7f59b3d
rein <reinX244@gmail.com>
2023-06-26 15:00:31 +0800

    Finished WelcomePage functionality, debugging functions of LoginPage for now.

--- JavaLoginSystemMyVersion/src/LoginPage.java
+++ JavaLoginSystemMyVersion/src/LoginPage.java
@@ -25,22 +25,22 @@ public class LoginPage extends JFrame implements ActionListener {
 
 	JLabel loginTitleLabel = new JLabel("LOGIN");
 
-	// TODO: align userIDLabel and userPasswordLabel
-	String userIDLabelString = String.format("%20s", "User ID:");
+	String userIDLabelString = String.format("%20s", "\tUser ID:");
 	String userPasswordLabelString = String.format("%20s", "Password:");
 
 	JLabel userIDLabel = new JLabel(userIDLabelString);
 	JLabel userPasswordLabel = new JLabel(userPasswordLabelString);
 
-	JButton loginButton = new JButton("Login");
-	JButton resetButton = new JButton("Reset");
+	JButton loginButton = new JButton("LOGIN");
+	JButton resetButton = new JButton("RESET");
 
 	JTextField userIDField = new JTextField();
 	JPasswordField userPasswordField = new JPasswordField();
 
 	JLabel loginStatusLabel = new JLabel();
 
-	Color backgroundPanelColor = new Color(224, 224, 224);
+	static Dimension mainPanelDimension = new Dimension(640, 480);
+	static Color backgroundPanelColor = new Color(224, 224, 224);
 
 	public LoginPage(HashMap<String, String> programIDsAndPasswords) {
 		this.programIDsAndPasswords = programIDsAndPasswords;
@@ -49,31 +49,29 @@ public class LoginPage extends JFrame implements ActionListener {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 
-		mainPanel.setPreferredSize(new Dimension(640, 480));
+		mainPanel.setPreferredSize(mainPanelDimension);
 		mainPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 40));
 		mainPanel.setBackground(backgroundPanelColor);
 
 		loginTitleLabel.setHorizontalAlignment(JLabel.CENTER);
-		loginTitleLabel.setPreferredSize(new Dimension(600, 50));
-		loginTitleLabel.setFont(new Font(null, Font.BOLD, 32));
+		loginTitleLabel.setPreferredSize(new Dimension(600, 40));
+		loginTitleLabel.setFont(new Font(null, Font.BOLD, 42));
 
 		mainPanel.add(loginTitleLabel);
 
-		userIDLabel.setPreferredSize(new Dimension(300, 50));
+		userIDLabel.setPreferredSize(new Dimension(250, 50));
 		userIDLabel.setFont(new Font(null, Font.PLAIN, 26));
-		userIDLabel.setHorizontalAlignment(JLabel.LEFT);
 
-		userIDField.setPreferredSize(new Dimension(300, 50));
+		userIDField.setPreferredSize(new Dimension(350, 50));
 		userIDField.setFont(new Font(null, Font.PLAIN, 26));
 
 		mainPanel.add(userIDLabel);
 		mainPanel.add(userIDField);
 
-		userPasswordLabel.setPreferredSize(new Dimension(300, 50));
+		userPasswordLabel.setPreferredSize(new Dimension(250, 50));
 		userPasswordLabel.setFont(new Font(null, Font.PLAIN, 26));
-		userPasswordLabel.setHorizontalAlignment(JLabel.LEFT);
 
-		userPasswordField.setPreferredSize(new Dimension(300, 50));
+		userPasswordField.setPreferredSize(new Dimension(350, 50));
 		userPasswordField.setFont(new Font(null, Font.PLAIN, 26));
 
 		mainPanel.add(userPasswordLabel);
@@ -81,14 +79,22 @@ public class LoginPage extends JFrame implements ActionListener {
 
 		loginButton.addActionListener(this);
 		loginButton.setFocusable(false);
-		loginButton.setPreferredSize(new Dimension(300, 50));
+		loginButton.setPreferredSize(new Dimension(250, 50));
+		loginButton.setFont(new Font(null, Font.BOLD, 26));
+		loginButton.setBorder(BorderFactory.createRaisedBevelBorder());
 
 		resetButton.addActionListener(this);
 		resetButton.setFocusable(false);
-		resetButton.setPreferredSize(new Dimension(300, 50));
+		resetButton.setPreferredSize(new Dimension(250, 50));
+		resetButton.setFont(new Font(null, Font.BOLD, 26));
+		resetButton.setBorder(BorderFactory.createRaisedBevelBorder());
 
 		mainPanel.add(loginButton);
 		mainPanel.add(resetButton);
+
+		loginStatusLabel.setPreferredSize(new Dimension(400, 50));
+		loginStatusLabel.setFont(new Font(null, Font.ITALIC, 36));
+		loginStatusLabel.setHorizontalAlignment(JLabel.CENTER);
 
 		mainPanel.add(loginStatusLabel);
 
@@ -109,13 +115,28 @@ public class LoginPage extends JFrame implements ActionListener {
 				// Getting password of userIDInput key in programIDsAndPasswords hashmap and
 				// checking if similar to userPasswordInput
 				if (programIDsAndPasswords.get(userIDInput).equals(userPasswordInput)) {
+					loginStatusLabel.setForeground(new Color(0, 128, 128));
 					loginStatusLabel.setText("Login Successful!");
+
+					try {
+						// TODO: fix this
+						JOptionPane.showMessageDialog(this, "Logging in...", "Login Message",
+								JOptionPane.INFORMATION_MESSAGE);
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} finally {
+						new WelcomePage(userIDInput);
+					}
+
 				} else {
 					// If the user types in the wrong password but right user ID
+					loginStatusLabel.setForeground(Color.RED);
 					loginStatusLabel.setText("Wrong Password!");
 				}
 			} else {
 				// User ID does not exit
+				loginStatusLabel.setForeground(Color.RED);
 				loginStatusLabel.setText("User ID Not Found!");
 			}
 		}
--- JavaLoginSystemMyVersion/src/WelcomePage.java
+++ JavaLoginSystemMyVersion/src/WelcomePage.java
@@ -1,4 +1,34 @@
 
-public class WelcomePage {
+import java.awt.BorderLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class WelcomePage extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	JPanel mainPanel = new JPanel();
+	JLabel welcomeMessageLabel = new JLabel();
+
+	public WelcomePage(String userIDString) {
+		this.setTitle("WELCOME");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+
+		mainPanel.setPreferredSize(LoginPage.mainPanelDimension);
+		mainPanel.setBackground(LoginPage.backgroundPanelColor);
+
+		welcomeMessageLabel.setText("Welcome " + userIDString);
+		welcomeMessageLabel.setFont(new Font(null, Font.BOLD, 42));
+
+		mainPanel.add(welcomeMessageLabel);
+
+		this.add(mainPanel);
+		this.pack();
+		this.setVisible(true);
+	}
 
 }
