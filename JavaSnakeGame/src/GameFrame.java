3f72430580647668d36b12f62b37145e75362f79
rein <reinX244@gmail.com>
2023-07-08 14:25:19 +0800

    Added Java Quiz Game jar and exe file. Added JavaSnakeGame project folder.

---
+++ JavaSnakeGame/src/GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+	public GameFrame() {
+
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.add(new GamePanel());
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+
+}
